57e8ace5180ff935d04f8c04314138b914dbc095
Novvea <anna.c.novak@gmail.com>
2021-01-25 12:15:44 +0100

    Initialized project with navigationbar

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './shared/css/Global.css'
+import { Routes } from './routes/Routes'
+import { DesktopNavigation } from './components/navigation/desktopnavigation/DesktopNavigation'
+
+function App() {
+	return (
+		<Routes>
+			<DesktopNavigation />
+		</Routes>
+	)
+}
+
+export default App
---
+++ src/view/AboutView.tsx
@@ -0,0 +1,7 @@
+export const AboutView = () => {
+	return (
+		<div>
+			<h1>This is the Aboutview</h1>
+		</div>
+	)
+}
